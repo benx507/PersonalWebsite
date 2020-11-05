c3868b4e5ea02890b50d33874c87d9d535e2d4e9
Benjamin Xiang <benx507@gmail.com>
2020-11-04 23:39:13 -0500

    added base landing page, need to make columns now

---
+++ benweb/src/homePage/lander.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+export default class Landing extends Component {
+    render() {
+        return(
+            <div>
+                Ben's First Try.
+                <p>
+                    Jessica is gonna be fine
+                </p>
+            </div>
+            
+        )
+    }
+}
